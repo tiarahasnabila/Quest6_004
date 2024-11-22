d08333562f5a1cf46a294f000f62239712ef8f27
tiarahasnabila <tiarahasnabila@gmail.com>
2024-11-22 16:25:05 +0700

    Menambahkan objek MataKuliah yang berisi daftar pilihan mata kuliah

---
+++ app/src/main/java/com/example/pertemuan8/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.pertemuan8.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
+}
