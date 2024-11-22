e45913d6153223b23e4c3065c0bf4fb08b171b3f
tiarahasnabila <tiarahasnabila@gmail.com>
2024-11-22 15:55:31 +0700

    Menambahkan ViewModel RencanaStudyViewModel untuk mengelola data rencana studi, termasuk fungsi untuk memperbarui mata kuliah yang dipilih

---
+++ app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.pertemuan8.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel : ViewModel(){
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUI: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
+    }
+
