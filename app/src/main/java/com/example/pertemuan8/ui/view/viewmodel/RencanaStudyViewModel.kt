803c85a5a1bc244f7ba288485824b49d7fcbc733
tiarahasnabila <tiarahasnabila@gmail.com>
2024-11-22 15:58:01 +0700

    menambahkan fungsi saveDataKRS untuk menyimpan data mata kuliah dan kelas ke dalam state KRS

--- app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -19,5 +19,11 @@ class RencanaStudyViewModel : ViewModel(){
         _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
     }
 
-
+    fun saveDataKRS(ls: MutableList<String>){
+        _krsState.update { status -> status.copy(
+            mataKuliah = ls[0],
+            kelas = ls[1]
+        ) }
+    }
+}
 
