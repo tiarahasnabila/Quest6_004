d6e9e6306ab3e16dc37b2656acba178cbe922417
tiarahasnabila <tiarahasnabila@gmail.com>
2024-11-22 16:12:06 +0700

    Menambahkan layout Column dan Row yang berisi logo UMY

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFormView.kt
@@ -59,4 +59,26 @@ fun MahasiswaFormView(
     val listData: MutableList<String> =
         mutableListOf(nim, nama, email)
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.logoumy
+                ),
+                contentDescription = "",
+                modifier = Modifier.size(45.dp)
+            )
+
 
