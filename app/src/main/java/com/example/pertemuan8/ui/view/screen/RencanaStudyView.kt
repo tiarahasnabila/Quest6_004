fea184e81ee98a856f4dff3f8a22005645baf92f
tiarahasnabila <tiarahasnabila@gmail.com>
2024-11-22 15:37:36 +0700

    Menambahkan tampilan formulir pemilihan mata kuliah dan kelas dengan teks, input dropdown, radio button, dan pembatas horizontal untuk mengatur pilihan peminatan.

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
@@ -109,6 +109,52 @@ fun RencanaStudiView(
                 )
                 .fillMaxSize(),
         ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ){
+                Text(text = "Pilih Mata Kuliah Peminatan", fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Silahkan pilih mata kuliah yang anda inginkan",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Light
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                DynamicSelectTextdField(
+                    selectedValue = chosenDropdown,
+                    options = MataKuliah.options,
+                    label = "Mata Kuliah",
+                    onValueChangedEvent = {
+                        chosenDropdown = it
+                    }
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                HorizontalDivider()
+                Spacer(modifier = Modifier.padding(8.dp))
+                Text(text = "Pilih Kelas Belajar", fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Silahkan pilih kelas dari mata kuliah yang anda inginkan",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Light
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ){
+                    RuangKelas.kelas.forEach { data ->
+                        Row (verticalAlignment = Alignment.CenterVertically){
+                            RadioButton(
+                                selected = pilihanKelas == data,
+                                onClick = { pilihanKelas = data}
+                            )
+                            Text(data)
+                        }
+                    }
+                }
+
+            }
 
         }
 
